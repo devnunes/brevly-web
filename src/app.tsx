85789f0a6adf8351e8cb5b3af7796a81e8a561cf
Matheus Nunes <matheusnunesinfo@gmail.com>
2025-06-30 19:55:43 -0300

    Responsiveness

--- src/app.tsx
+++ src/app.tsx
@@ -5,10 +5,14 @@ import { MyLinksWidget } from './components/my-links-widget'
 export function App() {
   return (
     <main className="h-dvh flex flex-col items-center p-10">
-      <div className='flex flex-start w-245 mb-4'>
-        <img src="../public/brevly-logo.svg" alt="Brev.ly Logo" className="w-24 h-7.5 mr-2" />
+      <div className="flex flex-start w-91.5 md:w-187 lg:w-245 mb-4">
+        <img
+          src="../public/brevly-logo.svg"
+          alt="Brev.ly Logo"
+          className="w-24 h-7.5 mr-2"
+        />
       </div>
-      <div className="flex items-center justify-center gap-5">
+      <div className="flex flex-col items-center justify-center gap-5 md:flex-row md:items-start md:gap-4">
         <CreateLinkWidget />
         <MyLinksWidget />
       </div>
--- src/components/my-links-widget.tsx
+++ src/components/my-links-widget.tsx
@@ -1,6 +1,6 @@
 export function MyLinksWidget() {
   return (
-    <div className="flex flex-col rounded-lg w-145 h-85 p-8 bg-gray-100 gap-6">
+    <div className="flex flex-col rounded-lg w-91.5 lg:w-145 h-85 p-8 bg-gray-100 gap-6">
       <span className="text-lg w-79">Meus links</span>
     </div>
   )
