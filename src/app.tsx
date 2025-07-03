224447e6fd373a0ce1e0251c23ca81e32d0bec88
Matheus Nunes <matheusnunesinfo@gmail.com>
2025-07-02 21:27:16 -0300

    Router

--- src/app.tsx
+++ src/app.tsx
@@ -1,21 +1,11 @@
 import './app.css'
-import { CreateLinkWidget } from './components/create-link-widget'
-import { MyLinksWidget } from './components/my-links-widget'
+import { BrowserRouter } from 'react-router-dom'
+import Router from '@/router'
 
 export function App() {
   return (
-    <main className="h-dvh flex flex-col items-center p-10">
-      <div className="flex justify-center mb-4 mt-10 w-91.5 md:w-187 lg:w-245 md:justify-start">
-        <img
-          src="./src/assets/brevly-logo.svg"
-          alt="Brev.ly Logo"
-          className="w-24 h-7.5 mr-2"
-        />
-      </div>
-      <div className="flex flex-col items-center justify-center gap-5 md:flex-row md:items-start md:gap-4">
-        <CreateLinkWidget />
-        <MyLinksWidget />
-      </div>
-    </main>
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
   )
 }
---
+++ src/pages/home.tsx
@@ -0,0 +1,20 @@
+import { CreateLinkWidget } from '@/components/widgets/create-link'
+import { MyLinksWidget } from '@/components/widgets/my-links'
+
+export default function Home() {
+  return (
+    <main className="h-dvh flex flex-col items-center p-10">
+      <div className="flex justify-center mb-4 mt-10 w-91.5 md:w-187 lg:w-245 md:justify-start">
+        <img
+          src="./src/assets/brevly-logo.svg"
+          alt="Brev.ly Logo"
+          className="w-24 h-7.5 mr-2"
+        />
+      </div>
+      <div className="flex flex-col items-center justify-center gap-5 md:flex-row md:items-start md:gap-4">
+        <CreateLinkWidget />
+        <MyLinksWidget />
+      </div>
+    </main>
+  )
+}
